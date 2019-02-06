42da43240ce8810d5694deac7433abbdda6d3c1c
Kumar <726124@cognizant.com>
2019-02-06 18:08:22 +0530

    GET /todos

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
         });
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find({})
+        .then((todos) => {
+            res.send({ todos })
+        })
+        .catch((err) => {
+            res.status(400).send(err)
+        })
+})
+
 app.listen(3000, () => {
     console.log(`connected successfully on port 3000.`);
 })
