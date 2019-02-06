1e9c38fa86182d2bb5ff401bdfd8b05d77ae89ae
Kumar <726124@cognizant.com>
2019-02-06 18:14:25 +0530

    test merge

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const bodyparser = require('body-parser')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./model/todo')
-const { User } = require('./model/user')
 
 const app = express()
 
