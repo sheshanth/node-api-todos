b7396a8ebc887336a5ec37c1aa048a98194c1c2d
Kumar <726124@cognizant.com>
2019-02-11 12:35:23 +0530

    hash password mongoose pre middleware

--- server/model/user.js
+++ server/model/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
 const _ = require('lodash')
+const bcrypt = require('bcryptjs')
 
 let userSchema = new mongoose.Schema({
     email: {
@@ -69,10 +70,19 @@ userSchema.statics.findByToken = function (token) {
     })
 }
 
-/* userSchema.pre('save', function (next) {
+userSchema.pre('save', function (next) {
     var user = this
-
-}) */
+    if(user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash
+                next()
+            })    
+        })
+    } else {
+        next()
+    }
+})
 
 let User = mongoose.model('User', userSchema)
 
